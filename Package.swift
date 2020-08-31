44afe1d71c4ae9a2e7cca7d23ccc807a2fc85bf8
RivieraKid <darrenwoodster@gmail.com>
2020-08-31 22:36:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Swift-YouTube-Player",
-            targets: ["Swift-YouTube-Player"]),
+            name: "YouTubePlayer",
+            targets: ["YouTubePlayer"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Swift-YouTube-Player",
+            name: "YouTubePlayer",
             path: "YouTubePlayer/YouTubePlayer/",
             dependencies: []),
     ]
